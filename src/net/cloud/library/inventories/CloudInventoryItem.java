3ed9bfe6d5fd7744a346335bbda1c12088d137a0
REDKILLERFARTS <thecodingduck1@gmail.com>
2020-12-03 12:28:19 -0500

    Changes to the CloudInventoryItem

--- src/net/cloud/library/inventories/CloudInventoryItem.java
+++ src/net/cloud/library/inventories/CloudInventoryItem.java
@@ -1,15 +1,23 @@
 package net.cloud.library.inventories;
 
+import net.cloud.library.inventories.enums.CloudInventoryItemType;
 import org.bukkit.inventory.ItemStack;
 
 public class CloudInventoryItem {
 
     public String name;
     public ItemStack item;
+    public CloudInventoryItemType type;
 
     public CloudInventoryItem(String name, ItemStack item) {
         setName(name);
         setItem(item);
+    }
+
+    public CloudInventoryItem(String name, ItemStack item, CloudInventoryItemType type) {
+        setName(name);
+        setItem(item);
+        setType(type);
     }
 
     public String getName() {
@@ -20,6 +28,10 @@ public class CloudInventoryItem {
         return item;
     }
 
+    public CloudInventoryItemType getType() {
+        return type;
+    }
+
     public CloudInventoryItem setName(String name) {
         this.name = name;
         return this;
@@ -27,6 +39,11 @@ public class CloudInventoryItem {
 
     public CloudInventoryItem setItem(ItemStack item) {
         this.item = item;
+        return this;
+    }
+
+    public CloudInventoryItem setType(CloudInventoryItemType type) {
+        this.type = type;
         return this;
     }
 
---
+++ src/net/cloud/library/inventories/enums/CloudInventoryItemType.java
@@ -0,0 +1,7 @@
+package net.cloud.library.inventories.enums;
+
+public enum CloudInventoryItemType {
+
+    NONE
+
+}
